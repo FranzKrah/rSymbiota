1bf401e0565ec4eac56dc0e9dc4b36212e8a1c2b
Franz <f.krah@mailbox.org>
2018-10-26 16:52:43 +0200

    added all collections

--- R/portals.R
+++ R/portals.R
@@ -11,8 +11,10 @@ portals <- function() {
     "Frullania Collaborative Research Network";"symbbryophytes";"http://bryophyteportal.org/frullania/";"http://bryophyteportal.org/frullania/collections/harvestparams.php?taxa=Frullania&reset=1"
     "Macroalgal Consortium Herbarium Portal";"symbalgae";"http://macroalgae.org/portal/index.php";"http://macroalgae.org/portal/collections/index.php"
     "MyCoPortal";"symbmycology";"http://mycoportal.org/portal/index.php";"http://mycoportal.org/portal/collections/index.php"
-    "Smithsonian Tropical Research Institute Portal (STRI)";"symbstri";"http://stricollections.org/portal/";
-    "Aquatic Invasives";"symbaquatic";"http://greatlakesinvasives.org/portal/index.php";
+    "Smithsonian Tropical Research Institute Portal (STRI)";"symbstri";"http://stricollections.org/portal/";"http://stricollections.org/portal/collections/index.php"
+    "Aquatic Invasives - Fish";"symbaquatic";"http://greatlakesinvasives.org/portal/index.php";"http://greatlakesinvasives.org/portal/collections/index.php?catid=3"
+    "Aquatic Invasives - Mollusk";"symbaquatic";"http://greatlakesinvasives.org/portal/index.php";"http://greatlakesinvasives.org/portal/collections/index.php?catid=4"
+    "Aquatic Invasives - Plant";"symbaquatic";"http://greatlakesinvasives.org/portal/index.php";"http://greatlakesinvasives.org/portal/collections/index.php?catid=1"
     "Consortium of Midwest Herbaria";"symbseinet";"http://midwestherbaria.org/portal/index.php";"http://midwestherbaria.org/portal/collections/index.php"
     "SEINet";"symbseinet";"http://swbiodiversity.org/seinet/index.php";"http://swbiodiversity.org/seinet/collections/index.php"
     "Intermountain Region Herbaria Network (IRHN)";"symbseinet";"http://intermountainbiota.org/portal/index.php";"http://intermountainbiota.org/portal/collections/index.php?catid=1"
@@ -22,28 +24,28 @@ portals <- function() {
     "Consortium of Northeastern Herbaria (CNH)";"symbcnh";"http://portal.neherbaria.org/portal/";"http://portal.neherbaria.org/portal/collections/index.php"
     "Madrean Archipelago Biodiversity Assessment (MABA) - Flora";"symbseinet";"http://madrean.org/symbflora/projects/index.php?proj=74";"http://madrean.org/symbflora/collections/index.php"
     "Madrean Archipelago Biodiversity Assessment (MABA) - Fauna";"symbfauna";"http://madrean.org/symbfauna/projects/index.php";"http://madrean.org/symbfauna/collections/index.php"
-    "Herbario Virtual Austral Americano";"symbhvaa";"http://herbariovaa.org/";
-    "CoTRAM – Cooperative Taxonomic Resource for Amer. Myrtaceae";"symbcotram";"http://cotram.org/";
-    "InvertEBase Data Portal";"symbinvertebase";"http://invertebase.org/portal/index.php";
+    "Herbario Virtual Austral Americano";"symbhvaa";"http://herbariovaa.org/";"http://herbariovaa.org/collections/index.php"
+    "CoTRAM – Cooperative Taxonomic Resource for Amer. Myrtaceae";"symbcotram";"http://cotram.org/";"http://cotram.org/collections/index.php?taxa=Myrtaceae"
+    "InvertEBase Data Portal";"symbinvertebase";"http://invertebase.org/portal/index.php";"http://invertebase.org/portal/collections/index.php"
     "Symbiota Collections of Arthropods Network (SCAN)";"symbscan";"http://symbiota4.acis.ufl.edu/scan/portal/index.php";"http://scan-bugs.org/portal/collections/index.php"
-    "Lepidoptera of North America Network (LepNet)";"symbscan";"http://symbiota4.acis.ufl.edu/scan/lepnet/portal/index.php";
-    "Neotropical Entomology";"symbneotropentomology";"http://symbiota.org/neotrop/entomology/index.php";
-    "Neotropical Flora";"symbneotropplants";"http://symbiota.org/neotrop/plantae/index.php";
-    "Monarch (California Academy of Sciences)";"internal network";"http://monarch.calacademy.org/";
-    "The Lundell Plant Diversity Portal";"?";"http://prc-symbiota.tacc.utexas.edu/index.php";
-    "Virtual Flora of Wisconsin";"?";"http://symbiota.botany.wisc.edu";
-    "Red de Herbarios del Noroeste de México";"symbseinet";"http://herbanwmex.net/portal/index.php";
-    "University of Colorado Herbarium";"?";"https://botanydb.colorado.edu/";
-    "The Open Herbarium";"symbhereb";"http://openherbarium.org/index.php";
-    "Consortium of Pacific Herbaria";"[own private database]";"http://www.pacificherbaria.org/";
-    "Oregon Flora";"Oregon State";"http://symbiota.oregonflora.org/portal/index.php";
-    "Minnesota Biodiversity Atlas";"";"http://bellatlas.umn.edu/";
-    "Documenting Ethnobiology in Mexico and Central America";"";"http://demca.sites.gettysburg.edu/";
-    "OpenZooMuseum";"symbzoo";"http://openherbarium.org/zoo/portal/index.php";
-    "Mid-Atlantic Herbaria Consortium";"";"http://midatlanticherbaria.org/portal/";
-    "Channel Islands Biodiversity Information System";"CAL-IBIS";"http://www.cal-ibis.org/";
-    "Consortium of Small Vertebrate Collections (CSVColl)";"?";"http://csvcoll.org/portal/index.php";
-    "The University of New Hampshire Collection of Insects and Other Arthropods";"UNH server";"https://unhcollection.unh.edu/database/index.php"'
+    "Lepidoptera of North America Network (LepNet)";"symbscan";"http://symbiota4.acis.ufl.edu/scan/lepnet/portal/index.php";"http://symbiota4.acis.ufl.edu/scan/lepnet/portal/collections/index.php"
+    "Neotropical Entomology";"symbneotropentomology";"http://symbiota.org/neotrop/entomology/index.php";"http://hasbrouck.asu.edu/neotrop/entomology/collections/index.php"
+    "Neotropical Flora";"symbneotropplants";"http://symbiota.org/neotrop/plantae/index.php";"http://hasbrouck.asu.edu/neotrop/plantae/collections/index.php"
+    "Monarch (California Academy of Sciences)";"internal network";"http://monarch.calacademy.org/";"https://monarch.calacademy.org/collections/index.php"
+    "The Lundell Plant Diversity Portal";"?";"http://prc-symbiota.tacc.utexas.edu/index.php";"https://prc-symbiota.tacc.utexas.edu/collections/index.php"
+    "Virtual Flora of Wisconsin";"?";"http://symbiota.botany.wisc.edu";"http://symbiota.botany.wisc.edu/collections/index.php"
+    "Red de Herbarios del Noroeste de México";"symbseinet";"http://herbanwmex.net/portal/index.php";"http://herbanwmex.net/portal/collections/index.php"
+    "University of Colorado Herbarium";"?";"https://botanydb.colorado.edu/";"https://botanydb.colorado.edu/collections/index.php"
+    "The Open Herbarium";"symbhereb";"http://openherbarium.org/index.php";"http://openherbarium.org/portal/collections/index.php"
+    "Consortium of Pacific Herbaria";"[own private database]";"http://www.pacificherbaria.org/";"http://www.pacificherbaria.org"
+    "Oregon Flora";"Oregon State";"http://symbiota.oregonflora.org/portal/index.php";"password required"
+    "Minnesota Biodiversity Atlas";"";"http://bellatlas.umn.edu/";"http://bellatlas.umn.edu/collections/index.php"
+    "Documenting Ethnobiology in Mexico and Central America";"";"http://demca.sites.gettysburg.edu/";"http://demca.sites.gettysburg.edu/collections/index.php"
+    "OpenZooMuseum";"symbzoo";"http://openherbarium.org/zoo/portal/index.php";"http://openherbarium.org/zoo/portal/collections/index.php"
+    "Mid-Atlantic Herbaria Consortium";"";"http://midatlanticherbaria.org/portal/";"http://midatlanticherbaria.org/portal/collections/index.php"
+    "Channel Islands Biodiversity Information System";"CAL-IBIS";"http://www.cal-ibis.org/";"http://www.cal-ibis.org/collections/index.php"
+    "Consortium of Small Vertebrate Collections (CSVColl)";"?";"http://csvcoll.org/portal/index.php";"http://csvcoll.org/portal/collections/index.php"
+    "The University of New Hampshire Collection of Insects and Other Arthropods";"UNH server";"https://unhcollection.unh.edu/database/index.php"; "https://unhcollection.unh.edu/database/collections/harvestparams.php"'
   )
   tab <- read.csv(text = tab, sep = ";")
   tab[] <- apply(tab, 2, as.character)
